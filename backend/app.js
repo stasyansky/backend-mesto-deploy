ae742d0b328088b3ae441490bd9a60745c222b74
Stanislav Ivanisov <st.ivanisov@yandex.ru>
2022-09-03 03:33:45 +0300

    third commit

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('express-rate-limit');
 const { celebrate, Joi, errors } = require('celebrate');
+const cors = require('cors');
 const { createUser, login } = require('./controllers/users');
 const { PATTERN_URL: pattern } = require('./utils/constants');
 const auth = require('./middlewares/auth');
@@ -11,24 +12,42 @@ const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const corsOptions = {
+  origin: [
+    'https://st.ivanisov.nomoredomains.sbs',
+    'http://st.ivanisov.nomoredomains.sbs',
+    'https://localhost:3001',
+    'http://localhost:3001',
+  ],
+  optionsSuccessStatus: 200,
+};
+
 const { PORT = 3000 } = process.env;
 const app = express();
+
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(requestLogger);
 app.use(limiter);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -49,6 +68,7 @@ app.post('/signin', celebrate({
 
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
+
 app.use(errorLogger);
 app.use('*', auth, () => {
   throw new NotFoundError('Страница не найдена');
